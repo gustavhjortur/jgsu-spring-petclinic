551cde53132c2b09913bdabb8c47db3f64af1770
Gustav H Gustavsson <gustav@tern.is>
2021-08-12 17:09:00 +0000

    test fail

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,6 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-#person.setFirstName("");
 		person.setFirstName("notempty");
 		person.setLastName("smith");
 
